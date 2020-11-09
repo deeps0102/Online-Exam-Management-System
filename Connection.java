87b3583d092c3eab4238bf231fabc67a75a9f620
Deepali Garg <gargdivya1002@gmail.com>
2020-11-10 04:36:38 +0530

    Update Connection.java

--- Connection.java
+++ Connection.java
@@ -1,7 +1,45 @@
 import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
  
 public class Connection 
-{}
+{
+	private Connection con;
+
+	public Connection() throws ClassNotFoundException, SQLException {
+        connectToDatabase();
+    }
+
+    private void connectToDatabase() throws ClassNotFoundException, SQLException {
+       
+            Class.forName("com.mysql.jdbc.Driver");
+            conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/exam_system","root","pswrd");
+       
+    }
+
+    public ArrayList getAllUsers(){
+        ArrayList list=new ArrayList();
+        User user=null;
+        PreparedStatement pstm;
+        try {
+            pstm = conn.prepareStatement("Select * from users");
+            ResultSet rs=pstm.executeQuery();
+            while(rs.next()){
+                user =new User(rs.getInt(1),rs.getString(2),
+                        rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10));
+            list.add(user);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            
+        }
+        return list;
+    }
+
+}
