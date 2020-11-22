acafb5ee40117707744d6bf30aaf743bf7d99d08
Deepali Garg <gargdivya1002@gmail.com>
2020-11-22 21:46:58 +0530

    Update Questions.java

--- base/Questions.java
+++ base/Questions.java
@@ -1,31 +1,42 @@
-public class Questions {
-    private int questionId;
-    private String question,opt1,opt2,opt3,opt4,correct,courseName;
+package base;
 
-    public Questions() {
-    }
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
 
-    public Questions(int questionId, String question, String opt1, String opt2, String opt3, String opt4, String correct, String courseName) {
-        this.setQuestionId(questionId);
+public class Questions
+{
+    public String queid, question, opt1, opt2, opt3, opt4, ans, examName, createdby,createddt;
+
+    public Questions() {}
+
+    public Questions(String queid, String question, String opt1, String opt2, String opt3, String opt4, String ans, String examName, String createdby, String createddt) 
+    {
+        this.setQuestionId(queid);
         this.setQuestion(question);
         this.setOpt1(opt1);
         this.setOpt2(opt2);
         this.setOpt3(opt3);
         this.setOpt4(opt4);
-        this.setCorrect(correct);
-        this.setCourseName(courseName);
+        this.setAns(ans);
+        this.setexamName(examName);
+        this.setcreatedby(createdby);
+        this.setcreateddt(createddt);
     }
 
-    public int getQuestionId() {
-        return questionId;
+    public String getQuestionId() {
+        return this.queid;
     }
 
-    public void setQuestionId(int questionId) {
-        this.questionId = questionId;
+    public void setQuestionId(String queid) {
+        this.queid = queid;
     }
 
     public String getQuestion() {
-        return question;
+        return this.question;
     }
 
     public void setQuestion(String question) {
@@ -33,7 +44,7 @@ public class Questions {
     }
 
     public String getOpt1() {
-        return opt1;
+        return this.opt1;
     }
 
     public void setOpt1(String opt1) {
@@ -41,7 +52,7 @@ public class Questions {
     }
 
     public String getOpt2() {
-        return opt2;
+        return this.opt2;
     }
 
     public void setOpt2(String opt2) {
@@ -49,7 +60,7 @@ public class Questions {
     }
 
     public String getOpt3() {
-        return opt3;
+        return this.opt3;
     }
 
     public void setOpt3(String opt3) {
@@ -57,28 +68,42 @@ public class Questions {
     }
 
     public String getOpt4() {
-        return opt4;
+        return this.opt4;
     }
 
     public void setOpt4(String opt4) {
         this.opt4 = opt4;
     }
 
-    public String getCorrect() {
-        return correct;
+    public String getAns() {
+        return this.ans;
     }
 
-    public void setCorrect(String correct) {
-        this.correct = correct;
+    public void setAns(String ans) {
+        this.ans = ans;
     }
 
-    public String getCourseName() {
-        return courseName;
+    public String getexamName() {
+        return this.examName;
     }
 
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
+    public void setexamName(String examName) {
+        this.examName = examName;
     }
     
+    public String getcreateddt() {
+        return this.createddt;
+    }
+
+    public void setcreateddt(String createddt) {
+        this.createddt = createddt;
+    }
     
+    public String getcreatedby() {
+        return this.createdby;
+    }
+
+    public void setcreatedby(String createdby) {
+        this.createdby = createdby;
+    }
 }
